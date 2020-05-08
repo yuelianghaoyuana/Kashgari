79fa0f21b934f3dfdc54cdd4abc53d2ba7bcabea
BrikerMan <eliyar917@gmail.com>
2020-05-08 17:26:11 +0800

    :sparkles: Add CNN_GRU/CNN_LSTM models.

--- kashgari/tasks/classification/__init__.py
+++ kashgari/tasks/classification/__init__.py
@@ -11,6 +11,8 @@ from .bi_lstm_model import BiLSTM_Model
 from .bi_gru_model import BiGRU_Model
 from .cnn_model import CNN_Model
 from .cnn_attention_model import CNN_Attention_Model
+from .cnn_gru_model import CNN_GRU_Model
+from .cnn_lstm_model import CNN_LSTM_Model
 
 if __name__ == "__main__":
     pass
